ad7d4108f9ff3d98425fbd0b529a29264be05241
Luis Angel Dilone <ldilone@aclanyc.com>
2017-04-29 20:44:56 +0000

    AAAA

---
+++ README.md
@@ -0,0 +1,12 @@
+# Team Y: Escape Egolab
+**Description:** Escape Egolab is an exploration game where the user must find and destroy a hidden camera. Along the way they must navigate between tables of ScriptEd volunteers and students.
+
+**Controls:** (**W**): Up
+              (**A**): Left
+              (**S**): Down
+              (**D**): Right
+
+**Message:** As it currently stands, the internet is a free and secure enviornment for all to use. However, it is not entirely clear what the _future_ holds. There have been many attempts to infringe upon users' rights, whether that be through charging for bandwidth throttling and marketing for targetted advertisements. Help keep the _future_ of the internet a better place by standing up for your rights and those of the less fortunate!
+
+**To Add:**
+*
---
+++ script.js
@@ -0,0 +1,154 @@
+// JavaScript/Jquery code goes here 
+/*global $*/
+// Arrays to create grid
+// Assigning each room true/false
+var room1 = [
+    [true, true, true, true, true],
+    [true, false, true, true, true],
+    [true, false, true, true, false],
+    [true, true, false, true, true],
+    [true, true, "exit", true, true]
+];
+
+var room2 = [
+    [true, true, true, true, true],
+    [true, false, true, false, true],
+    [true, true, true, true, true],
+    [true, false, true, false, true],
+    [true, true, true, true, true],
+];
+
+var room3 = [
+    [true, true, true, true, false],
+    [true, true, true, true, false],
+    [true, true, false, true, false],
+    [true, true, true, true, true],
+    [true, true, true, true, true]
+];
+
+var room4 = [
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, false, false, false, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true]
+];
+
+var room5 = [
+    [true, true, true, true, true],
+    [true, false, false, false, true],
+    [true, false, false, false, true],
+    [true, false, false, false, true],
+    [true, true, true, true, true]
+];
+
+var room6 = [
+    [true, true, true, true, true],
+    [true, false, false, false, true],
+    [true, true, true, true, true],
+    [true, false, false, false, true],
+    [true, true, true, true, true]
+];
+
+var room7 = [
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true]
+];
+
+var room8 = [
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, false],
+    [true, true, true, true, true],
+    [true, false, false, false, true]
+];
+
+var room9 = [
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, false, false, false, false],
+    [true, false, false, false, false]
+];
+
+var currentX = 0;
+var currentY = 0;
+var WCHAR = 87;
+var DCHAR = 68;
+var SCHAR = 83;
+var ACHAR = 65;
+var activeSquareId = "#x0y0";
+$(document).ready(function() {
+    $(activeSquareId).show();
+});
+
+$(document).keydown(function(e) {
+        if (e.keyCode === WCHAR) {
+            console.log("up");
+            var nextX = currentX - 1;
+            if (nextX >= 0 && nextX < 5) {
+                if (room1[nextX][currentY] === true) {
+                    currentX = currentX - 1;
+                }
+                //door code
+                else if (room1[currentX][currentY] === "door") {
+                    //change html page
+                }
+            }
+
+        }
+        else if (e.keyCode === DCHAR) {
+            console.log("right");
+            var nextY = currentY + 1;
+            if (currentY >= 0 && currentY < 5) {
+                console.log("inbounds");
+
+                if (room1[currentX][nextY] === true) {
+                    currentY = currentY + 1;
+                }
+                //door code
+                else if (room1[currentX][currentY] === "door") {
+                    //change html page
+                }
+            }
+        }
+        else if (e.keyCode === SCHAR) {
+            console.log("down");
+            var nextX = currentX + 1;
+            if (nextX >= 0 && nextX < 5) {
+                if (room1[nextX][currentY] === true) {
+                    currentX = currentX + 1;
+                }
+                //door code
+                else if (room1[currentX][currentY] === "door") {
+                    //change html page
+                }
+            }
+        }
+        else if (e.keyCode === ACHAR) {
+            console.log("left");
+            var nextY = currentY - 1;
+            if (currentY >= 0 && currentY < 5) {
+                if (room1[currentX][nextY] === true) {
+                    currentY = currentY - 1;
+                }
+                //door code
+                else if (room1[currentX][currentY] === "door") {
+                    //change html page
+                }
+            }
+        }
+
+        
+        var nextActiveSquareId = '#x' + currentX + 'y' + currentY;
+        console.log(activeSquareId);
+        $(activeSquareId).find(".hero").addClass("hidden");
+
+        activeSquareId = nextActiveSquareId;
+        console.log($(nextActiveSquareId).find(".hero"));
+        $(activeSquareId).find(".hero").removeClass("hidden");
+
+    });
